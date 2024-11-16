34bcf1f7d79b67f230b7ee0d69945a326dd46a00
Jonathan <many.d.r.o.2001@hotmail.com>
2024-11-16 16:08:45 -0500

    Bub report issue template created

--- src/controllers/deleteController.js
+++ src/controllers/deleteController.js
@@ -3,11 +3,10 @@ import { deleteTask } from "../services/taskService.js";
 async function deleteController(id) {
     try {
         if (id == undefined) throw new Error('No arguments');
-        if (isNaN(id) || !Number.isInteger(parseInt(id))) throw new TypeError('id must be an integer number');
+        if (Number.isNaN(id) || !Number.isInteger(parseInt(id))) throw new TypeError('id must be an integer number');
         await deleteTask(parseInt(id));
     } catch (error) {
         throw new Error(error.message);
-        //console.error(error.message);
     }
 }
 
